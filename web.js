445a39dad17cb3b6b7dc596c7d06e2c7474d6b2f
Vandan Mehta <vandan.s.mehta@gmail.com>
2013-07-12 06:16:44 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var file = "index.html";
+  var buffer = new Buffer(256);
+  var fs = require('fs');
+  var b = fs.readFileSync(file);	
+  response.send(b.toString());
 });
 
 var port = process.env.PORT || 5000;
